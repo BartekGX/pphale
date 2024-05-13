6f6e4ec3a4cbc543c40942df934ed99f8cee8d89
XkotX <bartekbrotek81@gmail.com>
2024-05-13 20:33:25 +0200

    naprawa photoSlidera

--- src/components/imageslider.js
+++ src/components/imageslider.js
@@ -1,7 +1,7 @@
 "use client";
 import { useState } from "react";
 import { ChevronLeftIcon, ChevronRightIcon } from "@heroicons/react/24/solid";
-import ImageZoom from "react-image-zooom";
+import ImageZoom from "@/components/react-image-zooom";
 
 export default function ImageSlider({ urls }) {
     const transformedUrls = urls.map(url => {
@@ -32,16 +32,19 @@ export default function ImageSlider({ urls }) {
                     return (
                         <div
                             key={image}
-                            className="w-full h-full flex-shrink-0 flex-grow-0 transition-transform ease-in-out duration-300 relative flex justify-center items-center"
+                            className="w-full h-full flex-shrink-0 transition-transform ease-in-out duration-300 relative flex justify-center items-center basis-auto"
                             style={{
                                 transform: `translateX(-${100 * imageIndex}%)`,
                                 zIndex: index === imageIndex ? 10 : 0,
                             }}
                         >
                             <ImageZoom
-                                className="block flex-shrink-0 flex-grow-0 max-w-full max-h-full overflow-hidden object-fill select-none cursor-zoom-in"
+                                className="block flex-shrink-0 flex-grow-0 w-auto h-full overflow-hidden object-contain select-none cursor-zoom-in fullImageZoom basis-auto"
                                 src={image}
                                 alt={`img${index + 1}`}
+                                zoom="250"
+                                width="100%"
+                                height="100%"
                             />
                         </div>
                     );
@@ -51,15 +54,16 @@ export default function ImageSlider({ urls }) {
                 onClick={showPrevImg}
                 className="z-20 absolute group-hover:sm:translate-x-[32px] -left-[32px] translate-x-[32px] h-full flex top-0 justify-center items-center bg-black bg-opacity-50 p-1 sm:opacity-50 opacity-30 hover:opacity-100 transition-all"
             >
-                <ChevronLeftIcon className="h-6 w-6 text-white" />
+                <ChevronLeftIcon className="h-6 w-6 text-white"/>
             </button>
             <button
                 onClick={showNextImg}
                 className="z-20 absolute group-hover:sm:-translate-x-[32px] -right-[32px] -translate-x-[32px] h-full flex top-0 justify-center items-center bg-black bg-opacity-50 p-1 sm:opacity-50 opacity-30 hover:opacity-100 transition-all"
             >
-                <ChevronRightIcon className="h-6 w-6 text-white" />
+                <ChevronRightIcon className="h-6 w-6 text-white"/>
             </button>
-            <div className="z-20 absolute top-0 left-1/2 -translate-x-1/2 text-white bg-black bg-opacity-50 p-1 rounded-y-lg rounded-b-lg">
+            <div
+                className="z-20 absolute top-0 left-1/2 -translate-x-1/2 text-white bg-black bg-opacity-50 p-1 rounded-y-lg rounded-b-lg">
                 {imageIndex + 1} z {urls.length}
             </div>
         </div>
