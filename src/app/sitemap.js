5c5de3cd403353e947793974e76f271e550cfae9
BartekGX <bartekgrotek8@gmail.com>
2024-05-24 22:00:20 +0200

    sitemap update

--- src/app/sitemap.js
+++ src/app/sitemap.js
@@ -1,5 +1,21 @@
+const getMeta = async () => {
+    let res = [];
+    try {
+        res = await fetch(`${process.env.API_URL}/api/offeru/meta`)
+            .then(response => response.json())
+            .catch(e => {
+                console.error(e);
+                return [];
+            });
+    } catch (e) {
+        console.error(e);
+        res = [];
+    }
+    return res;
+};
+
 export default async function sitemap() {
-    const data = await fetch(`${process.env.API_URL}/api/offeru/meta`).then((res) => res.json())
+    const data = await getMeta()
     const products = data.map(item => ({
         url: `${process.env.API_URL}/${item.reference}`,
         lastModified: new Date(item.updatedAt)
