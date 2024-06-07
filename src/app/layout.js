d478fb2c4da939c834e99d6d8617e181872bfaab
BartekGX <bartekgrotek8@gmail.com>
2024-06-07 10:06:00 +0200

    Update layout.js

--- src/app/layout.js
+++ src/app/layout.js
@@ -1,5 +1,6 @@
 import { Inter } from "next/font/google";
 import "./globals.css";
+import Script from "next/script"
 
 const inter = Inter({ subsets: ["latin"] });
 export const metadata = {
@@ -10,6 +11,21 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
       <html lang="pl" className={inter.className}>
+    <head>
+    <Script 
+      async 
+      src="https://www.googletagmanager.com/gtag/js?id=G-0MZ6NF8EDV"
+    ></Script>
+    <Script id="google-analytics">
+      {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+    
+      gtag('config', 'G-0MZ6NF8EDV');
+    `}
+</Script>
+    </head>
       <body>
               <main>
                   {children}
