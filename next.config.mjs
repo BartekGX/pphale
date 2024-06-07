a49c79c61303818f1fe20c8b1aa1b64ad6542fb0
BartekGX <bartekgrotek8@gmail.com>
2024-06-07 18:13:27 +0200

    Update next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -8,6 +8,12 @@ const nextConfig = {
                 port: '',
                 pathname: '/*',
             },
+             {
+                protocol: 'https',
+                hostname: 'autolandstorage.s3.eu-north-1.amazonaws.com',
+                port: '',
+                pathname: '/*',
+            },
         ],
     },
     env: {
